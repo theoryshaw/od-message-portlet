4b336a4f9db61ef31eb9f7ae437aa229f64ac84d
Chris Oloff <chris@francois-pc.magmatec.net>
2011-05-19 15:19:46 +0200

    Removed unnecessary db stuff

--- docroot/WEB-INF/sql/tables.sql
+++ docroot/WEB-INF/sql/tables.sql
@@ -1,18 +1,11 @@
-create table PR_PRProduct (
-	productId LONG not null primary key,
-	productName VARCHAR(75) null,
-	serialNumber VARCHAR(75) null,
+create table OD_QueryAndResponse (
+	uuid_ VARCHAR(75) null,
+	queryId LONG not null primary key,
+	parentId LONG,
+	title VARCHAR(1000) null,
+	url VARCHAR(1000) null,
+	createdAt DATE null,
 	companyId LONG,
-	groupId LONG
-);
-
-create table SK_SKQuestion (
-	Question_ID LONG not null primary key,
-	Parent_ID LONG,
-	Title VARCHAR(75) null,
-	Url STRING null,
-	Post_Date LONG,
-	User_ID LONG,
-	companyId LONG,
-	groupId LONG
+	groupId LONG,
+	userId LONG
 );
